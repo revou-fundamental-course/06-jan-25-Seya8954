a33eef443d3ec8f25ee84265659fbf78192ba65e
Seya8954 <usersayatauy@gmail.com>
2025-01-17 20:34:32 +0700

    create index

--- js/script.js
+++ js/script.js
@@ -5,52 +5,38 @@ console.log('Hello World!');
 //fungsi untuk memvalidasi form
 function formValidation() {
     let nameInput = document.getElementById('name').value;
+    let emailInput = document.getElementById('email').value;
+    let messageInput = document.getElementById('message').value;
 
     console.log(nameInput);
     // kondisi untuk memvalidasi inputan nama
     if (nameInput === '') {
         alert('Name must be filled out');
-    } else {
+    } 
+    else {
         //menampilkan nama inputan
         document.getElementById('result-form').innerHTML = nameInput;
-    }
-
-    //menampilkan nama inputan
-    document.getElementById('result-form').innerHTML = nameInput;
-}
-
-document.getElementById('submit-btn').addEventListener('click', formValidation)
-
-
-function formValidation() {
-    let nameInput = document.getElementById('name').value;
-    let emailInput = document.getElementById('email').value;
-    let messageInput = document.getElementById('message').value;
-
-    if (nameInput === '') {
-        alert('Name must be filled out');
-        return;
     }
 
     if (emailInput === '') {
         alert('Email must be filled out');
         return;
+    } 
+    else {
+        //menampilkan nama inputan
+        document.getElementById('result-form').innerHTML = nameInput;
     }
 
     if (messageInput === '') {
         alert('Message must be filled out');
         return;
     }
+    else {
+        //menampilkan nama inputan
+        document.getElementById('result-form').innerHTML = nameInput;
+    }
 
+    //menampilkan nama inputan
+    document.getElementById('result-form').innerHTML = nameInput; emailInput; messageInput;
     document.getElementById('result-form').innerHTML = `Name: ${nameInput}<br>Email: ${emailInput}<br>Message: ${messageInput}`;
 }
-
-document.getElementById('submit-btn').addEventListener('click', function() {
-    let nameInput = document.getElementById('name').value;
-
-    if (nameInput === '') {
-        alert('Name must be filled out');
-    } else {
-        formValidation();
-    }
-});
